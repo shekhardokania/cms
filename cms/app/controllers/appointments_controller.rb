d8a2109294e8009f00905aec5cf8f5f2bb98c4d4
shekhar <shekhar.pesit@gmail.com>
2014-02-28 16:40:22 +0530

    appointment resize reschedule

--- cms/app/controllers/appointments_controller.rb
+++ cms/app/controllers/appointments_controller.rb
@@ -5,10 +5,6 @@ class AppointmentsController < ApplicationController
    @appointment = Appointment.new
   end
 
-  def patient_name_for_autocomplete
-    render :json => current_user.patients.where("name like ? ", "%#{params[:term]}%").map {|x| {:label => x.name,:value => x.name,:id => x.id }}
-  end
-
   def new
     @appointment = Appointment.new
   end
@@ -17,6 +13,17 @@ class AppointmentsController < ApplicationController
     appointment = Appointment.find(params[:id])
     doctor = appointment.doctor
     patient = appointment.patient
+  end
+
+  def create
+    new_patient = Patient.create(:name => params[:patient_name])   if params[:patient_id].blank?
+    appointment = Appointment.new(:doctor_id => current_user.id,:patient_id => params[:patient_id] ,:start_time => params[:start_date],:end_time => params[:end_date],:allday => params[:allday],:title => params[:patient_name])
+    appointment.patient_id = new_patient.id if params[:patient_id].blank?
+    if appointment.save
+      redirect_to :back, notice: "Appointment created successfully.#{' Please complete the patient profile.' if new_patient}" and return
+    else
+      format.html { redirect_to :back, notice: appointment.errors.full_messages.join("</br>") }
+    end
   end
 
   def reschedule
@@ -28,20 +35,13 @@ class AppointmentsController < ApplicationController
      end
   end
 
-  def create
-   new_patient = Patient.create(:name => params[:patient_name])   if params[:patient_id].blank?
-   appointment = Appointment.new(:doctor_id => current_user.id,:patient_id => params[:patient_id] ,:start_time => params[:start_date],:end_time => params[:end_date],:allday => params[:allday],:title => params[:patient_name])
-   appointment.patient_id = new_patient.id if params[:patient_id].blank?
-   if appointment.save
-     redirect_to :back, notice: "Appointment created successfully.#{' Please complete the patient profile.' if new_patient}" and return
-   else
-     format.html { redirect_to :back, notice: appointment.errors.full_messages.join("</br>") }
-   end
-  end
-
   def get_appointments
     #appointments = Appointment.all
-    render :json => current_user.appointments.map {|a| {:id => a.id,:title => a.title,:start => a.start_time.to_i,:end => a.end_time.to_i,:allDay => false,:type => 'follow up',:url => url_for(a)}}
+    render :json => current_user.appointments.map {|a| {:id => a.id,:title => a.title,:start => a.start_time.to_i,:end => a.end_time.to_i,:allDay => false,:type => 'follow up',:url => path_for(a)}}
+  end
+
+  def patient_name_for_autocomplete
+    render :json => current_user.patients.where("name like ? ", "%#{params[:term]}%").map {|x| {:label => x.name,:value => x.name,:id => x.id }}
   end
 
 end
